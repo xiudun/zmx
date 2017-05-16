90be5a4e4271ab1371d8f4e1d25b517723995669
xiudun <747297277@qq.com>
2017-05-16 09:27:50 +0800

    第二次使用git

---
+++ git001/src/com/zmx/action/Test.java
@@ -0,0 +1,19 @@
+/**
+ * @(#)Test.java
+ * Description:	TODO 填写文件作用简要说明
+ * Version :	0.0.0
+ * Copyright:	Copyright (c) 哈尔滨修盾信息科技有限公司  版权所有
+ * Create by:	张茂修  2017年5月16日
+ */
+package com.zmx.action;
+
+/**
+ * TODO 填写功能说明
+ * @author 张茂修
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+}
